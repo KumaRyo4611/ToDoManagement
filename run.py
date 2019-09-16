fe9c11577530f0175539319b5c0b1d6c4c426c9f
KumaRyo4611 <49774431+KumaRyo4611@users.noreply.github.com>
2019-09-16 23:53:12 +0900

    Initial commit

---
+++ run.py
@@ -0,0 +1,38 @@
+import responder
+import sqlite3
+import json
+
+api = responder.API()
+
+@api.route("/getAll")
+def get_all(req, resp):
+    conn = sqlite3.connect('testDB.sqlite3')
+    c = conn.cursor()
+    c.execute("SELECT * FROM tasks")
+    data = c.fetchall()
+    conn.commit()
+    conn.close()
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.media = data
+    print(data)
+
+@api.route("/addData")
+def add_data(req, resp):
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    conn = sqlite3.connect('testDB.sqlite3')
+    c = conn.cursor()
+    c.execute("insert into tasks values (?, ?, ?, ?, ?, ?, ?)", (2, 'タスクB', '2019-09-04', 'タスクBを完了させる', 'プライベート', '', 0))
+    conn.commit()
+    conn.close()
+
+@api.route("/deleteData/{id}")
+def delete_data(req, resp, *, id):
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    conn = sqlite3.connect('testDB.sqlite3')
+    c = conn.cursor()
+    c.execute('DELETE FROM tasks WHERE id = ?', id)
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    api.run()
---
+++ src/main.js
@@ -0,0 +1,106 @@
+window.onload = function() {
+    vm.todos.length = 0;
+    // XMLHttpRequestオブジェクトの作成
+    var request = new XMLHttpRequest();
+
+    // URLを開く
+    request.open('GET', "http://127.0.0.1:5042/getAll", true);
+    request.responseType = 'json'
+
+    // レスポンスが返ってきた時の処理を記述 
+    request.onload = function () {
+        vm.todos.length = 0;
+        var data = this.response;
+        for (var i = 0; i < data.length; ++i) {
+            var task = data[i];
+            console.log(task);
+            var recode = { id: task[0], tasks: task[1], limit: task[2], detail: task[3], category: task[4], remarks: task[5] };
+            vm.todos.push(recode);
+        }
+    }
+
+    // リクエストをURLに送信
+    request.send();
+}
+
+var vm = new Vue({
+    el: '.app-table',
+    data: {
+        sortOf: 0,
+        sortType: true,
+        todos: [
+        ]
+    },
+    computed: {
+        sortedTasks: function() {
+            this.sortOf
+            return this.todos.sort(this.cmp)
+        }
+    },
+    methods: {
+        cmp: function(a, b) {
+            switch (this.sortOf) {
+                case 0:
+                    return this.sortType ? a.id - b.id : b.id - a.id
+                case 1:
+                    if (this.sortType) {
+                        return (a.tasks > b.tasks) ? 1 : -1
+                    } else {
+                        return (a.tasks < b.tasks) ? 1 : -1
+                    }
+                case 2:
+                    if (this.sortType) {
+                        return (a.limit > b.limit) ? 1 : -1
+                    } else {
+                        return (a.limit < b.limit) ? 1 : -1
+                    }
+                case 3:
+                    if (this.sortType) {
+                        return (a.category > b.category) ? 1 : -1
+                    } else {
+                        return (a.category < b.category) ? 1 : -1
+                    }
+                default :
+                    break
+            }
+        },
+        dispdetail: function(todo) {
+            detailvm.dispflg = true;
+            detailvm.todo = todo;
+        },
+        
+    }
+})
+
+var detailvm = new Vue({
+    el: '.detail',
+    data: {
+        dispflg: false,
+        addflg: false,
+        todo: { id: 0, tasks: 'タスク', limit: '期限', detail: '詳細', category: 'カテゴリ', remarks: '備考' }
+    },
+    methods: {
+        remove: function(todo) {
+            // XMLHttpRequestオブジェクトの作成
+            var request = new XMLHttpRequest();
+
+            // URLを開く
+            var delURL = "http://127.0.0.1:5042/deleteData/" + this.todo.id;
+            request.open('GET', delURL, true);
+            request.responseType = 'json'
+
+            // レスポンスが返ってきた時の処理を記述 
+            request.onload = function () {
+                window.onload;
+            }
+
+            // リクエストをURLに送信
+            request.send();
+        }
+    }
+})
+
+function add() {
+    detailvm.dispflg = true;
+    detailvm.addflg = true;
+}
